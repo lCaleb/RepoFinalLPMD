1cf16a1cc188f0e71ac991abfd754bab88b38440
1016101782 <1016101782@SC208E07.academico.icesi.edu.co>
2018-09-21 18:30:12 -0500

    se agregó en principal

--- ProyectoAPIO2/src/modelo/Principal.java
+++ ProyectoAPIO2/src/modelo/Principal.java
@@ -3,7 +3,10 @@ package modelo;
 public class  Principal {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo :) ");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hola Mundo :) ");
+		}
+		
 	}
 
 }
